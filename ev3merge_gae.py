461fa765b8f55c4cd506c3f784fbd88fb35ccb5c
Your Name <you@example.com>
2014-10-01 00:34:35 -0400

    Add file handling to app engine code

--- ev3merge_gae.py
+++ ev3merge_gae.py
@@ -5,6 +5,9 @@ import os
 import ev3_merge
 import webapp2
 import jinja2
+import zipfile
+import StringIO
+import cgi
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
@@ -17,7 +20,19 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         context = {}
-        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render(context))
+
+    def post(self):
+        ev3files = [f.file for f in self.request.POST.multi.getall('file')]
+        
+        merged = ev3files[0]
+        merged.read()
+        merged.seek(0)
+
+        self.response.headers['Content-type'] = 'application/octet-stream'
+        self.response.headers['Content-Disposition'] = 'attachment; filename=merged.ev3'
+        self.response.write(merged.read())
+        
 
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
