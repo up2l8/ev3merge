323d2ee83f1f8b2722a71faf6ae2c6050debdb88
Eric Miller <eric@millerfam.net>
2014-10-01 12:39:25 -0400

    Remove jinja, using only static html for now. Add required to input files field

--- ev3merge_gae.py
+++ ev3merge_gae.py
@@ -1,27 +1,16 @@
 
 #vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 import os
-
-from ev3 import Ev3
 import webapp2
-import jinja2
-import zipfile
 import StringIO
-import cgi
+from ev3 import Ev3
 
-TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+HTML_DIR = os.path.join(os.path.dirname(__file__), 'static', 'html')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        context = {}
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render(context))
+        self.response.write(open(os.path.join(HTML_DIR, 'index.html')).read())
 
     def post(self):
 	upload_files = self.request.POST.multi.getall('file')
@@ -33,11 +22,12 @@ class MainPage(webapp2.RequestHandler):
             name, _ = os.path.splitext(upload_file.filename)
 	    merged.merge(Ev3(upload_file.file, name=name))
 
-        self.response.headers['Content-type'] = 'application/octet-stream'
-        self.response.headers['Content-Disposition'] = 'attachment; filename=merged.ev3'
 	outfile = StringIO.StringIO()
 	merged.write(outfile)
 	outfile.seek(0)
+        
+        self.response.headers['Content-type'] = 'application/octet-stream'
+        self.response.headers['Content-Disposition'] = 'attachment; filename=merged.ev3'
         self.response.write(outfile.read())
 
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
